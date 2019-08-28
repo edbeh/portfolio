0503778925a7d4f0223053bebed53b72f068c853
edbeh89 <edbeh89@gmail.com>
2019-08-28 17:49:53 +0800

    update 28.8.19 changed to upwork contact

--- src/components/FirstPage/Contact/Contact.js
+++ src/components/FirstPage/Contact/Contact.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './Contact.css';
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faComments } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Contact extends React.Component {
     render() {
         return (  
-            <a href="mailto:contact@edwardbeh.com" className="rectangle">
-                <div className="notification-text">
-                    <FontAwesomeIcon icon={faEnvelope} size="lg"/>
-                    <span>&nbsp;contact@edwardbeh.com</span>
-                </div>
-            </a>
+            // <a href="mailto:contact@edwardbeh.com" className="rectangle">
+            //     <div className="notification-text">
+            //         <FontAwesomeIcon icon={faEnvelope} size="lg"/>
+            //         <span>&nbsp;contact@edwardbeh.com</span>
+            //     </div>
+            // </a>
+            <a 
+                href="https://www.upwork.com/freelancers/~01258e6b7e5fd37215" 
+                className="rectangle"
+                target="_blank"
+            >
+            <div className="notification-text">
+                <FontAwesomeIcon icon={faComments} size="lg"/>
+                <span>&nbsp;Contact Me on Upwork!</span>
+            </div>
+        </a>
         );
     }
 }
