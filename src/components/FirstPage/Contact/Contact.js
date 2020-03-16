b83d32e9a4c6342c6b998aef78696fdf7c022925
edbeh <42892950+edbeh@users.noreply.github.com>
2020-03-16 16:26:26 +0800

    update contact.js

--- src/components/FirstPage/Contact/Contact.js
+++ src/components/FirstPage/Contact/Contact.js
@@ -6,12 +6,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class Contact extends React.Component {
     render() {
         return (  
-            // <a href="mailto:contact@edwardbeh.com" className="rectangle">
-            //     <div className="notification-text">
-            //         <FontAwesomeIcon icon={faEnvelope} size="lg"/>
-            //         <span>&nbsp;contact@edwardbeh.com</span>
-            //     </div>
-            // </a>
             <a 
                 href="mailto:edbeh89@gmail.com" 
                 className="rectangle"
